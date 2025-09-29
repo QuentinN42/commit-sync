a414bf740522488a4d6a6451fd6a5642e7a322cf
QuentinN42 <quentin@lieumont.fr>
2025-09-29 17:18:07 +0200

    feat: update script for auto run
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- script.sh
+++ script.sh
@@ -1,18 +1,38 @@
-#!/bin/sh
-# Create a git repo on github
-# Put this script inside
-# Update USERNAME
-# Run this script
-# Push
+#!/usr/bin/env bash
+
+if [ -z "${GITLAB_USERNAME}" ]; then
+    echo "USERNAME is not set"
+    exit 1
+fi
+if [ -z "${REPOSITORY}" ]; then
+    echo "REPOSITORY is not set"
+    exit 1
+fi
+if [ -z "${GITHUB_USERNAME}" ]; then
+    echo "GITHUB_USERNAME is not set"
+    exit 1
+fi
+if [ -z "${GITHUB_EMAIL}" ]; then
+    echo "GITHUB_EMAIL is not set"
+    exit 1
+fi
+
+cd "$(mktemp -d)"
+dir_path="$(pwd)"
+trap "rm -rf ${dir_path}" EXIT
+git clone "${REPOSITORY}" "repo"
+cd "repo"
+
+git config user.name "${GITHUB_USERNAME}"
+git config user.email "${GITHUB_EMAIL}"
 
 PRE_COMMIT_MSG='auto: '
-USERNAME='QuentinN42'
 
 function modif(){
     cat changes | grep -q "true" && echo "false" > changes || echo "true" > changes
 }
 
-todo="$(curl -s "https://gitlab.com/users/${USERNAME}/calendar.json" | jq -rc '. | to_entries | .[] | "\(.key)-\(range(.value)+1)"')"
+todo="$(curl -s "https://gitlab.com/users/${GITLAB_USERNAME}/calendar.json" | jq -rc '. | to_entries | .[] | "\(.key)-\(range(.value)+1)"')"
 already_done="$(git log --pretty='format:%s' | grep -E "^${PRE_COMMIT_MSG}" | sed "s/^${PRE_COMMIT_MSG}//")"
 
 export IFS=$'\n'
@@ -22,6 +42,7 @@ do
     if [[ ! $already_done =~ $x ]]; then
         modif
         git add changes
-        git commit -s -m "${PRE_COMMIT_MSG}${x}" --date="$(date -d "$(echo "${x}" | cut -d- -f-3)")"
+        commit_date="$(date -d "$(echo "${x}" | cut -d- -f-3)" '+%Y-%m-%d %H:%M:%S')"
+        GIT_COMMITTER_DATE="${commit_date}" git commit -s -m "${PRE_COMMIT_MSG}${x}" --date "${commit_date}"
     fi
 done
